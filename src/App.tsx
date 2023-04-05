2d037a50a52a3e19d80fe0d492a5d005005ff1b4
tomasyovino <tomasyovino@gmail.com>
2023-04-05 12:31:16 -0300

    Display Reacr Router & Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { CssBaseline, ThemeProvider } from "@mui/material"
+import { CssBaseline, ThemeProvider, Box } from "@mui/material"
 import { createTheme } from "@mui/material/styles"
 import { useMemo } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { themeSettings } from "./theme"
+import Navbar from "@/scenes/navbar"
 
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
   return <div className="app">
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<div>Dashboard Page</div>} />
+            <Route path="/predictions" element={<div>Predictions Page</div>} />
+          </Routes>
+        </Box>
+      </ThemeProvider>
+    </Router>
   </div>
 }
 
